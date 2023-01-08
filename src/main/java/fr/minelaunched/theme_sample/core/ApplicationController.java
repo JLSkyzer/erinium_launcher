445a096df4b48385919c3e73d16c2144a1b982ff
JLSkyzer <killiandasneves4@gmail.com>
2023-01-08 21:34:32 +0100

    backup

--- src/main/java/fr/minelaunched/theme_sample/core/ApplicationController.java
+++ src/main/java/fr/minelaunched/theme_sample/core/ApplicationController.java
@@ -5,6 +5,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -14,7 +17,6 @@ import javafx.stage.StageStyle;
 
 import java.net.URL;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ApplicationController implements Initializable {
     private Stage stage;
@@ -30,19 +32,22 @@ public class ApplicationController implements Initializable {
 
     @FXML
     private Button btnHome;
-
     @FXML
     private Button btnNewsML;
-
     @FXML
     private StackPane stackpane;
-
     @FXML
     private ImageView backgroundframe;
+    @FXML
+    private AnchorPane anchorepane;
+    @FXML
+    private Button app_close_btn;
+    @FXML
+    private Button app_reduce_btn;
 
     public ApplicationController(Stage stage) {
         this.stage = stage;
-        stage.setTitle("Erinium World");
+        stage.setTitle("Erinium Launcher");
         stage.setWidth(1280);
         stage.setHeight(720);
         stage.initStyle(StageStyle.TRANSPARENT);
@@ -60,9 +65,14 @@ public class ApplicationController implements Initializable {
 
         Object backgroundImg = dataFields.get("background");
         Object backgroundframeImg = dataFields.get("backgroundframe");
+        /*---------- Button ----------*/
+        Object closebutton = dataFields.get("closebutton");
+        Object closebuttonhover = dataFields.get("closebuttonhover");
+        Object reducebutton = dataFields.get("reducebutton");
+        Object reducebuttonhover = dataFields.get("reducebuttonhover");
+
         if (dataFields.containsKey("background")){
             String backgroundImgString = new String(backgroundImg.toString());
-            Image image = new Image(backgroundImgString);
             Background bgimage = new Background(new BackgroundImage(new Image(backgroundImgString), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(1280, 720, true, true, true, true) /*BackgroundSize.DEFAULT*/));
             stackpane.setBackground(bgimage);
         }
@@ -70,7 +80,25 @@ public class ApplicationController implements Initializable {
             String backgroundFrameImgString = new String(backgroundframeImg.toString());
             Image image = new Image(backgroundFrameImgString);
             backgroundframe.setImage(image);
-        }else{System.out.println("Set backgroundframe to default (No value)");}
+        }
+        if (dataFields.containsKey("closebutton")) {
+            String closebuttonString = new String(closebutton.toString());
+            String closebuttonhoverString = new String(closebuttonhover.toString());
+            Background closebtnimage = new Background(new BackgroundImage(new Image(closebuttonString), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(35, 35, false, false, true, false)));
+            Background closebtnhoverimage = new Background(new BackgroundImage(new Image(closebuttonhoverString), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(35, 35, false, false, true, false)));
+            app_close_btn.setBackground(closebtnimage);
+            app_close_btn.setOnMouseEntered(e -> app_close_btn.setBackground(closebtnhoverimage));
+            app_close_btn.setOnMouseExited(e -> app_close_btn.setBackground(closebtnimage));
+        }
+        if (dataFields.containsKey("reducebutton")) {
+            String reducebuttonString = new String(reducebutton.toString());
+            String reducebuttonhoverString = new String(reducebuttonhover.toString());
+            Background reducebuttonimage = new Background(new BackgroundImage(new Image(reducebuttonString), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(35, 35, false, false, true, false)));
+            Background reducebuttonhoverimage = new Background(new BackgroundImage(new Image(reducebuttonhoverString), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(35, 35, false, false, true, false)));
+            app_reduce_btn.setBackground(reducebuttonimage);
+            app_reduce_btn.setOnMouseEntered(e -> app_reduce_btn.setBackground(reducebuttonhoverimage));
+            app_reduce_btn.setOnMouseExited(e -> app_reduce_btn.setBackground(reducebuttonimage));
+        }
     }
 
     private Image toString(Object value) {
@@ -103,12 +131,48 @@ public class ApplicationController implements Initializable {
 
     public void onNewsMLBtnClicked() {
         AnchorPane statutservermc_container = (AnchorPane) stage.getScene().lookup("#statutservermc_container");
+        ScrollPane newsScrollPane = (ScrollPane) stage.getScene().getRoot().lookup("#newsScrollPane");
         VBox news_container = (VBox) stage.getScene().lookup("#news_container");
         boolean show = !news_container.isVisible();
         setNewsContainerShown(show);
 
         statutservermc_container.setManaged(true);
         statutservermc_container.setVisible(true);
+
+        Object newstitlelabel = dataFields.get("newstitlebar");
+        Object newshbox = dataFields.get("newshbox");
+        Object newstextarea = dataFields.get("newstextarea");
+
+        if (dataFields.containsKey("newstitlebar")){
+            /*Title Label*/
+            String titlelabelColorString = new String(newstitlelabel.toString());
+            Color titlelabelColor = Color.web(titlelabelColorString);
+            titlelabelColor = titlelabelColor.deriveColor(0, 1.0, 1.0, 0.8);
+            /*newshbox*/
+            String newshboxString = new String(newshbox.toString());
+            Color newshboxColor = Color.web(newshboxString);
+            newshboxColor = newshboxColor.deriveColor(0, 1.0, 1.0, 0.8);
+            /*newstextarea*/
+            String newstextareaString = new String(newstextarea.toString());
+            Color newstextareaColor = Color.web(newstextareaString);
+            newstextareaColor = newstextareaColor.deriveColor(0, 1.0, 1.0, 0.8);
+
+            for(Node node : anchorepane.getScene().getRoot().lookupAll("#title_label") ){
+                Label label = (Label) node;
+                label.setBackground(new Background(new BackgroundFill(titlelabelColor, CornerRadii.EMPTY, Insets.EMPTY)));
+//                node.setStyle("-fx-background-color: " + titlelabelColor + ";");
+            }
+            for(Node node : anchorepane.getScene().getRoot().lookupAll("#newshbox") ){
+                HBox hbox = (HBox) node;
+                hbox.setBackground(new Background(new BackgroundFill(newshboxColor, CornerRadii.EMPTY, Insets.EMPTY)));
+//                node.setStyle("-fx-background-color: " + titlelabelColor + ";");
+            }
+            for(Node node : anchorepane.getScene().getRoot().lookupAll("#content_text") ){
+                TextArea hbox = (TextArea) node;
+                hbox.setBackground(new Background(new BackgroundFill(newstextareaColor, CornerRadii.EMPTY, Insets.EMPTY)));
+//                node.setStyle("-fx-background-color: " + titlelabelColor + ";");
+            }
+        }
     }
 
     private void setSettingsContainerShown(boolean show) {
